bbb8e32ec49862b464158f4cbaca13d1a3432a90
MayukhJha <mayukh0005@gmail.com>
2024-10-03 12:27:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function rolldice(){
+    const numofdice=document.getElementById("numofdice").value;
+    const diceresult=document.getElementById("diceresult");
+    const diceimages=document.getElementById("diceimages");
+    const values=[];
+    const images=[];
+    for(let i=0;i<numofdice;i++){
+        const value=Math.floor(Math.random()*6)+1;
+        values.push(value);
+        images.push(`<img src="images/${value}.jpg" alt="DICE : ${value}">`)
+    }
+    diceresult.textContent=`DICE : ${values.join(`, `)}`
+    diceimages.innerHTML=images.join(``)
+}
